a1333d44f704e2994085984463c62cddfdfdf793
ander <e1ias@ya.ru>
2016-07-11 16:17:13 +0300

    Add login page

---
+++ views/news/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="views/news/theme.css">
+</head>
+<body>
+    <div class="page">
+        <div class="content-left">
+            <form action="index.php?auth" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label>Логин:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="login" size="40">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Пароль:</label>
+                        </td>
+                        <td>
+                            <input type="password" name="passwd" size="40">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="right">
+                            <input type="submit" value="Войти" />
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+         <div class="content-right">
+            <ul class="menu">
+                <li><a href="index.php">На главную</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+
+
