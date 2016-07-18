e753e064dc9c6f3438ddadd06936290b00e854b7
ander <e1ias@ya.ru>
2016-07-18 12:26:07 +0300

    Change front controller. Add error handler

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
 session_start();
+require_once __DIR__ . '/core/autoload.php';
 
-require_once __DIR__ . '/autoload.php';
+use app\core\libraries\Router;
+use app\core\mvc\View;
 
-$ctrlName = Router::findController();
-$actName  = Router::findAction();
 
-$ctrl = new $ctrlName;
-$ctrl->$actName();
+$router = new Router();
+$ctrlName = 'app\controllers\\' . $router->getController();
+$actName  = $router->getAction();
+
+
+try {
+    $ctrl = new $ctrlName;
+    $page = $ctrl->$actName();
+    View::showPage($page);
+} catch (Exception $e) {
+    $view = new View();
+    $page = $view->render('errors/other', ['e' => $e]);
+    View::showPage($page);
+}
 
 
