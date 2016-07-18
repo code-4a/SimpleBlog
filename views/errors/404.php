47b99a6c22fa3db270f7079bd93096f899477f1f
ander <e1ias@ya.ru>
2016-07-18 12:24:05 +0300

    Add html files for view errors

---
+++ views/errors/404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="views/posts/theme.css">
+</head>
+<body>
+    <h1>Page not found</h1>
+</body>
+</html>
+
---
+++ views/errors/other.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="views/posts/theme.css">
+</head>
+<body>
+    <h1>Неизвестная ошибка</h1>
+    <p><?= $e.message ?></p>
+</body>
+</html>
+
